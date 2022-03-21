a312a3e45965c7355a226769b3c9c7695a1b9087
bun913 <imanau03@gmail.com>
2022-03-21 10:56:00 +0900

    feat: 一旦ログインのUIだけ作成

---
+++ app/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-main>
+      <LogIn/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import LogIn from './components/Login';
+
+export default {
+  name: 'App',
+
+  components: {
+    LogIn,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
